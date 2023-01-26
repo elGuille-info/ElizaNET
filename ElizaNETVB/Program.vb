b5c0ba8d686aed4891bb5f83563d28824b977a3b
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2023-01-26 17:24:45 +0100

    Usando cuadros de diálogo de TaskDialog

---
+++ ElizaNETVB/Program.vb
@@ -0,0 +1,24 @@
+﻿'--------------------------------------------------------------------------------
+' Punto de entrada de ElizaNETVB.                               (26/ene/23 17.21)
+'
+' (c) Guillermo Som (Guille), 2023
+'--------------------------------------------------------------------------------
+Option Strict On
+Option Infer On
+
+Imports System
+Imports System.Windows.Forms
+
+Friend Module Program
+
+    <STAThread()>
+    Friend Sub Main(args As String())
+        Application.SetHighDpiMode(HighDpiMode.SystemAware)
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+
+        Application.Run(New fEliza())
+
+    End Sub
+
+End Module
